fad81837be3720fe8110a3c14b1932ed28334e6c
nicolasgonzalez26 <nicolas.gonzalez.ospina.201368@gmail.com>
2026-08-27 17:37:59 -0500

    Se mejoro el codigo de division -Nico Gonzalez

--- division.py
+++ division.py
@@ -1,4 +1,7 @@
 num1 = float(input("Ingrese el número 1: "))
 num2 = float(input("Ingrese el número 2: "))
-division = num1 / num2
+if num2 == 0:
+    print("Eso no es posible hermano, ingrese otro número")
+else:
+    division = num1 / num2
 print("El resultado es: ", division)
