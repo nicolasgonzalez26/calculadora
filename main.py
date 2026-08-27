343659708b33335ed58f0ed995f86daae0eeee62
Nicolas Vera Vizcaya <nicolas.vera@estudiantesunibague.edu.co>
2026-08-27 17:16:00 -0500

    Agrega funcion de multiplicacion

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 num1 = int(input("Ingrese el número 1: "))
 num2 = int(input("Ingrese el número 2: "))
 division = num1 / num2
 print("El resultado es: ", division)
+=======
+def multiplicacion(numero1, numero2):
+    return numero1 * numero2
+
+print(multiplicacion(5, 4))
+>>>>>>> 6b0357a (Agrega funcion de multiplicacion)
